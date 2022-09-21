35990d75ba7d5259831e92518e19e26278fbe450
Vadim Sovetnikov <vadimsovetnikov@mail.ru>
2022-09-21 09:57:19 +0300

    controller added

--- src/main/java/com/game/controller/PlayerController.java
+++ src/main/java/com/game/controller/PlayerController.java
@@ -3,9 +3,8 @@ package com.game.controller;
 import com.game.entity.Player;
 import com.game.service.PlayerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,12 +15,50 @@ public class PlayerController {
     private final PlayerService playerService; //внедряем сервис
 
     @Autowired //автосвязка
-    public PlayerController(PlayerService playerService){
+    public PlayerController(PlayerService playerService) {
         this.playerService = playerService;
     }
 
     @GetMapping() //по запросу по условию
-    public List<Player> getPlayers(){
+    public List<Player> getPlayers() {
+        return playerService.findAll();
+    }
+
+    @GetMapping("/count")
+    public Integer count() {
+        return playerService.findAll().size();
+    }
+
+    @PostMapping()
+    public Player create(@ModelAttribute("player") Player player,
+                         BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return null;
+        }
+
+        playerService.save(player);
+        return playerService.findOne(player.getId());
+    }
+
+    @GetMapping("/{id}")
+    public Player getOne(@PathVariable("id") long id, Player player) {
+        return playerService.findOne(id);
+    }
+
+    @PostMapping("/{id}")
+    public Player update(@ModelAttribute("player") Player player,
+                         BindingResult bindingResult, @PathVariable("id") long id) {
+        if (bindingResult.hasErrors()) {
+            return null;
+        }
+
+        playerService.update(id, player);
+        return playerService.findOne(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public List<Player> delete(@PathVariable("id") long id){
+        playerService.delete(id);
         return playerService.findAll();
     }
 }
